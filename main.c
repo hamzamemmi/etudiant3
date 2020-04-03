4c368d84ee31e0837370d628eee3e37504bd8584
firaschkoundali <firas.chkoundali@esprit.tn>
2020-04-03 14:06:52 -0700

    c'est mon main

---
+++ main.c
@@ -0,0 +1,40 @@
+#include "SDL/SDL.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+#include "vie.h"
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
